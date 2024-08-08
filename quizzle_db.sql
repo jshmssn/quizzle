de35562f304901c9ef49e3568a777fb0474bc020
Joshua Mission <joshuamiguel_mission@sdca.edu.ph>
2024-08-08 10:44:32 +0800

    updated db

--- quizzle_db.sql
+++ quizzle_db.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.11-MariaDB - mariadb.org binary distribution
+-- Host:                         10.0.0.66
+-- Server version:               8.0.32 - MySQL Community Server - GPL
 -- Server OS:                    Win64
 -- HeidiSQL Version:             12.5.0.6677
 -- --------------------------------------------------------
@@ -14,13 +14,67 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping database structure for quizzle
+CREATE DATABASE IF NOT EXISTS `quizzle` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `quizzle`;
 
--- Data exporting was unselected.
+-- Dumping structure for table quizzle.answers
+CREATE TABLE IF NOT EXISTS `answers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `question_id` int NOT NULL,
+  `answer_text` varchar(255) NOT NULL,
+  `is_correct` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table quizzle.answers: ~4 rows (approximately)
+DELETE FROM `answers`;
+
+-- Dumping structure for table quizzle.participants
+CREATE TABLE IF NOT EXISTS `participants` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `room_pin` varchar(50) NOT NULL,
+  `scores` int DEFAULT NULL,
+  `isAnswered` int DEFAULT '0',
+  `isValid` int DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table quizzle.participants: ~0 rows (approximately)
+DELETE FROM `participants`;
+
+-- Dumping structure for table quizzle.questions
+CREATE TABLE IF NOT EXISTS `questions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `question_text` varchar(255) NOT NULL,
+  `room_pin` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `time` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table quizzle.questions: ~1 rows (approximately)
+DELETE FROM `questions`;
+
+-- Dumping structure for table quizzle.rooms
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `room_id` varchar(10) NOT NULL,
+  `pin` varchar(50) NOT NULL,
+  `isValid` int DEFAULT '1',
+  `hasStarted` int DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table quizzle.rooms: ~1 rows (approximately)
+DELETE FROM `rooms`;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
