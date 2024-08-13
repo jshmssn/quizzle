f142a1b0674e56433074242e505eca8b3caed49e
Joshua Mission <joshuamiguel_mission@sdca.edu.ph>
2024-08-13 15:40:27 +0800

    db update

--- quizzle_db.sql
+++ quizzle_db.sql
@@ -30,9 +30,9 @@ CREATE TABLE IF NOT EXISTS `answers` (
   PRIMARY KEY (`id`),
   KEY `question_id` (`question_id`),
   CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=258 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table quizzle.answers: ~4 rows (approximately)
+-- Dumping data for table quizzle.answers: ~0 rows (approximately)
 DELETE FROM `answers`;
 
 -- Dumping structure for table quizzle.participants
@@ -43,37 +43,44 @@ CREATE TABLE IF NOT EXISTS `participants` (
   `scores` int DEFAULT NULL,
   `isAnswered` int DEFAULT '0',
   `isValid` int DEFAULT '1',
+  `joined_at` timestamp NULL DEFAULT NULL,
+  `leave_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table quizzle.participants: ~0 rows (approximately)
+-- Dumping data for table quizzle.participants: ~1 rows (approximately)
 DELETE FROM `participants`;
 
 -- Dumping structure for table quizzle.questions
 CREATE TABLE IF NOT EXISTS `questions` (
   `id` int NOT NULL AUTO_INCREMENT,
   `question_text` varchar(255) NOT NULL,
-  `room_pin` varchar(50) DEFAULT NULL,
+  `image_path` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `room_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `time` int DEFAULT NULL,
+  `isValid` int DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table quizzle.questions: ~1 rows (approximately)
+-- Dumping data for table quizzle.questions: ~0 rows (approximately)
 DELETE FROM `questions`;
 
 -- Dumping structure for table quizzle.rooms
 CREATE TABLE IF NOT EXISTS `rooms` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `room_id` varchar(10) NOT NULL,
-  `pin` varchar(50) NOT NULL,
+  `room_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `pin` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `isValid` int DEFAULT '1',
   `hasStarted` int DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `room_id` (`room_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table quizzle.rooms: ~1 rows (approximately)
+-- Dumping data for table quizzle.rooms: ~0 rows (approximately)
 DELETE FROM `rooms`;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
